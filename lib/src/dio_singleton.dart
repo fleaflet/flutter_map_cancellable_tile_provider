cb83b6d6634870ff0c1d3ec66ad52c4857500380
corepuncher <58479168+corepuncher@users.noreply.github.com>
2024-10-04 15:00:35 -0500

    Update dio_singleton.dart
    - NativeAdapter  (cupertino http) required for iOS.
    - Cronet may or may not work with Android. There are problems when too many parallel requests. As such, only iOS platforms are currently using the native package.

--- lib/src/dio_singleton.dart
+++ lib/src/dio_singleton.dart
@@ -1,14 +1,22 @@
+import 'dart:io'; // Platform.is
 import 'package:flutter/foundation.dart'; // kIsWeb
 import 'package:dio/dio.dart';
 
 /// Remove if kIsWeb or won't compile
 import 'package:native_dio_adapter/native_dio_adapter.dart';
 
+class GetPlatform {
+  static const bool isWeb = kIsWeb;
+  static final bool isIOS = !kIsWeb && (Platform.isIOS || Platform.isMacOS);
+  static final bool isAndroid = !kIsWeb && Platform.isAndroid;
+  static final bool isWindows = !kIsWeb && Platform.isWindows;
+}
+
 class DioSingleton {
   static final Dio _dio = Dio();
 
   static Dio get dioInstance {
-    if (!kIsWeb) {
+    if (GetPlatform.isIOS) {
       _dio.httpClientAdapter = NativeAdapter();
     }
     return _dio;
